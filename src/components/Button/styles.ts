8f071de0df505b381c3961afbef242965b3e0739
Nicoly Sampaio <78435150+nicolysampaio@users.noreply.github.com>
2022-10-30 19:03:43 -0300

    refactor: css helper

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 import { TouchableOpacity } from "react-native";
 
 export type ButtonTypeStyleProps = "PRIMARY" | "SECONDARY";
@@ -25,8 +25,10 @@ export const Container = styled(TouchableOpacity)<Props>`
 export const Title = styled.Text`
   text-align: center;
 
-  font-family: ${({ theme }) => theme.FONT_FAMILY.BOLD};
-  font-size: ${({ theme }) => theme.FONT_SIZE.MD}px;
-  color: ${({ theme }) => theme.COLORS.WHITE};
+  ${({theme}) => css`
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.MD}px;
+    color: ${theme.COLORS.WHITE};
+  `}
 `;
 
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,5 +1,5 @@
-import { TextInput } from 'react-native';
-import styled from 'styled-components/native';
+import { TextInput } from "react-native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
   flex: 1;
@@ -7,9 +7,11 @@ export const Container = styled(TextInput)`
   min-height: 56px;
   max-height: 56px;
 
-  background-color: ${({theme}) => theme.COLORS.GRAY_700};
-  color: ${({ theme }) => theme.COLORS.WHITE};
-
   border-radius: 6px;
   padding: 16px;
+
+  ${({ theme }) => css`
+    background-color: ${theme.COLORS.GRAY_700};
+    color: ${theme.COLORS.WHITE};
+  `}
 `;
